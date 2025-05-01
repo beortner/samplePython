806a908f8f20684e8901dc7ef3c021f2afd2d436
beortner <bernhardortner@gmx.net>
2025-05-01 11:18:20 +0200

    inital commit

---
+++ noxfile.py
@@ -0,0 +1,21 @@
+import nox
+
+@session(python=False)
+def tests(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "report")
+
+@nox.session(python=False)
+def lint(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("black", "--check", ".")
+    session.run("flake8", ".")
+
+@nox.session(python=False)
+def typing(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("mypy", ".")
